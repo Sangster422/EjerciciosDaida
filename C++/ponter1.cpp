3cbbd0f61e6ee3ec6aa19ba82230626577dbe76a
Sangster422 <andreu.sangstersales@gmail.com>
2025-11-10 18:51:12 +0100

    changeVarName

--- C++/ponter1.cpp
+++ C++/ponter1.cpp
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main() {
     int var = 20;   // actual variable declaration
-    int *ip;        // pointer variable
+    int *address;        // pointer variable
 
-    ip = &var;     // store address of var in pointer variable
+    address = &var;     // store address of var in pointer variable
 
     printf("Value of var variable: %d\n", var);
     printf("Address of var variable: %p\n", (void*)&var);
-    printf("Address stored in ip variable: %p\n", (void*)ip);
-    printf("Value pointed to by ip variable: %d\n", *ip);
+    printf("Address stored in address variable: %p\n", (void*)address);
+    printf("Value pointed to by address variable: %d\n", *address);
 
     return 0;
 }

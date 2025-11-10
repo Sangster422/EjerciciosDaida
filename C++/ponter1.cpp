fdeeb813c542f76988342a6e5e7fe08efb613a4b
Sangster422 <andreu.sangstersales@gmail.com>
2025-11-10 18:57:44 +0100

    nowVarIsNum

--- C++/ponter1.cpp
+++ C++/ponter1.cpp
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main() {
-    int var = 20;   // actual variable declaration
+    int num = 20;   // actual variable declaration
     int *ip;        // pointer variable
 
-    ip = &var;     // store address of var in pointer variable
+    ip = &num;     // store address of var in pointer variable
 
-    printf("Value of var variable: %d\n", var);
-    printf("Address of var variable: %p\n", (void*)&var);
+    printf("Value of var variable: %d\n", num);
+    printf("Address of var variable: %p\n", (void*)&num);
     printf("Address stored in ip variable: %p\n", (void*)ip);
     printf("Value pointed to by ip variable: %d\n", *ip);
 
